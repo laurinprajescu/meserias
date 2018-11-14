7e63b8028e90bcb21965d8d10d101a3122c7bb93
laurinprajescu <laurinprajescu@gmail.com>
2018-11-14 17:55:16 +0000

    startover

--- dorel/dorel/wsgi.py
+++ dorel/dorel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for meserias project.
+WSGI config for dorel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meserias.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dorel.settings')
 
 application = get_wsgi_application()
